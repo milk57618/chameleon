c66bf3a795ef0665307db237cb024bf8d7dece5a
kibeom <lkbest0085@gmail.com>
2021-05-16 03:09:47 +0900

    test

--- kibeom/report.py
+++ kibeom/report.py
@@ -3,7 +3,7 @@ def sendData(h,t):
     data = {'humid': h, 
             'temp': t , 
             'time': '2021051623123232'}
-    URL = 'http://luvbeenhere.com/report'
+    URL = 'http://luvbeenhere.com:5000/report'
     res = requests.post(URL, data=json.dumps(data))
     print(res.text)
 
